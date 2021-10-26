857b6a3530e0af5582448b62748ef266201fc2ae
Mehmet Baykar <mbaykar@userlytics.com>
2021-10-26 23:13:31 +0300

    Refactor codebase Add corners to top bar add new variables to customise the UI

--- Sources/PullableBottomBar/PullableBottomBar.swift
+++ Sources/PullableBottomBar/PullableBottomBar.swift
@@ -1,70 +1,89 @@
 import UIKit
 
 open class PullableBottomBar: UIViewController {
-
+    
+    open var backgroundColor:UIColor = .clear
+    
+    open var roundTopBar = true
+    
     open var snapPoints: [SnapPoint] = [.min, .max] {
         didSet { snapPoints.sort() }
     }
-
+    
     private var pullableMinY: CGFloat {
         return snapPoints.first?.y ?? SnapPoint.min.y
     }
-
+    
     private var pullableMaxY: CGFloat {
         return snapPoints.last?.y ?? SnapPoint.max.y
     }
-
+    
     private let topBarStyle: TopBarStyle
     private let contentViewController: UIViewController?
     
-    private var status = PullableBottomBarStatus.expand
+    private var position = PullableBottomBarStatus.shrink
     
     private var parentView: UIView?
     private weak var contentScrollView: UIScrollView?
-
+    
+    deinit{
+        self.parentView = nil
+        self.contentScrollView = nil
+        print("\(Self.description()) has been deinited")
+    }
     public init(content: UIViewController,
-                topBarStyle: TopBarStyle = .default) {
+                topBarStyle: TopBarStyle = .default,
+                position:PullableBottomBarStatus = .shrink) {
         self.contentViewController = content
         self.topBarStyle = topBarStyle
+        self.position = position
         super.init(nibName: nil, bundle: nil)
     }
-
+    
     required public init?(coder aDecoder: NSCoder) {
-        contentViewController = nil
-        topBarStyle = .default
+        self.contentViewController = nil
+        self.topBarStyle = .default
         super.init(coder: aDecoder)
-        setupTopBarTapGesture()
     }
-
+    
     override open func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
-        setupTopBarTapGesture()
     }
-
+    
     private func setupViews() {
-        view.backgroundColor = .clear
-        view.layer.cornerRadius = 5
+        setupBackgroundView()
+        setupTapBar()
+        setupTopBarTapGesture()
+        setupContainerView()
+    }
+    
+    private func setupBackgroundView(){
+        view.backgroundColor = self.backgroundColor
         view.clipsToBounds = true
-
-        let gesture = UIPanGestureRecognizer(target: self, action: #selector(panGesture))
-        gesture.delegate = self
-        self.view.addGestureRecognizer(gesture)
-
+        view.frame.origin.y = self.position == .expand ? pullableMinY : pullableMaxY
+        setupPanGesture()
+    }
+    
+    
+    private func setupTapBar(){
         let topBar = topBarStyle.view
         view.addSubview(topBar)
         topBar.center.x = view.center.x
-        topBar.autoresizingMask = [.flexibleLeftMargin, .flexibleRightMargin]
+        topBar.autoresizingMask = [.flexibleLeftMargin, .flexibleRightMargin,.flexibleWidth]
         topBar.isMultipleTouchEnabled = false
-
+    }
+    
+    private func setupContainerView(){
         if let content = contentViewController {
             addContainerView(content)
             content.view.translatesAutoresizingMaskIntoConstraints = false
+            
             NSLayoutConstraint.activate([
-                content.view.topAnchor.constraint(equalTo: topBar.bottomAnchor, constant: 0),
+                content.view.topAnchor.constraint(equalTo: self.topBarStyle.view.bottomAnchor),
                 content.view.leftAnchor.constraint(equalTo: view.leftAnchor),
                 content.view.rightAnchor.constraint(equalTo: view.rightAnchor)
-                ])
+            ])
             
             if #available(iOS 11.0, *) {
                 content.view.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
@@ -72,38 +91,46 @@ open class PullableBottomBar: UIViewController {
                 content.view.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
             }
         }
-        self.view.bringSubviewToFront(topBar)
-        self.view.frame.origin.y = pullableMaxY
+        self.view.bringSubviewToFront(topBarStyle.view)
+        
     }
-
+    private func setupTopBarTapGesture(){
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.didTopBarTapped))
+        self.topBarStyle.view.addGestureRecognizer(tapGesture)
+    }
+    
+    private func setupPanGesture(){
+        let gesture = UIPanGestureRecognizer(target: self, action: #selector(panGesture))
+        gesture.delegate = self
+        self.view.addGestureRecognizer(gesture)
+    }
+    
+    @objc private func didTopBarTapped(){
+        switch self.position{
+        case .shrink: self.expand()
+        case .expand: self.shrink()
+        default:break
+    
+        }
+    }
+    
     open override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         guard let view = parentView else { return }
         self.view.frame.size.height = view.frame.height - pullableMinY
+        if roundTopBar{ topBarStyle.view.roundCorners([.topLeft,.topRight], radius: 20) }
+        
     }
-
+    
     open override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
         super.willTransition(to: newCollection, with: coordinator)
         self.updatePositions()
     }
     
     open func updatePositions(){}
-
-    private func setupTopBarTapGesture(){
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.didTopBarTapped))
-        self.topBarStyle.view.addGestureRecognizer(tapGesture)
-    }
     
-    @objc private func didTopBarTapped(){
-        switch self.status{
-        case .shrink: self.expand()
-        case .expand: self.shrink()
-        default:break
-        }
-        
-    }
     open func show(on viewController: UIViewController,
-                  view: UIView? = nil) {
+                   view: UIView? = nil) {
         parentView = view ?? viewController.view
         viewController.addContainerView(self, view: view)
         self.view.autoresizingMask = [.flexibleWidth,
@@ -111,44 +138,36 @@ open class PullableBottomBar: UIViewController {
                                       .flexibleHeight,
                                       .flexibleBottomMargin]
     }
-
-    open func scroll(toY y: CGFloat, duration: Double = 0.75) {
-        UIView.animate(withDuration: duration, delay: 0.0, usingSpringWithDamping: 0.75, initialSpringVelocity: 0, options: [], animations: {
-            self.view.frame.origin.y = y
-        }, completion: nil)
-    }
     
-    
-    
-    open func updateStatus(status:PullableBottomBarStatus){
-        guard self.status != status else {return}
-        
-    }
-    
-    open func expand(){
-        guard self.status == .shrink else {return}
-        
-        scroll(toY: pullableMinY)
-        self.status = .expand
-        self.contentViewController?.viewDidAppear(true)
-    }
-    
-    open func shrink() {
-       guard self.status == .expand else {return}
-        
-        scroll(toY: pullableMaxY)
-        self.status = .shrink
-        self.contentViewController?.viewDidDisappear(false)
-    }
-
     open func scrollViewDidScroll(_ scrollView: UIScrollView) {
         self.contentScrollView = scrollView
-
+        
         if scrollView.contentOffset.y < 0 {
             scrollView.contentOffset.y = 0
         }
     }
+}
 
+extension PullableBottomBar{
+    
+    open func scroll(toY y: CGFloat,
+                     duration: Double = 0.75,
+                     completion: ((Bool) -> Void)? = nil) {
+        UIView.animate(withDuration: duration, delay: 0.0, usingSpringWithDamping: 0.75, initialSpringVelocity: 0, options: [.allowUserInteraction], animations: {
+            self.view.frame.origin.y = y
+        }, completion: completion)
+    }
+    
+    open func expand(){
+        scroll(toY: pullableMinY, duration: 0.75)
+        self.position = .expand
+    }
+    
+    open func shrink(){
+        scroll(toY: pullableMaxY, duration: 0.75)
+        self.position = .shrink
+    }
+    
     @objc private func panGesture(_ recognizer: UIPanGestureRecognizer) {
         
         defer { recognizer.setTranslation(.zero, in: view) }
@@ -169,25 +188,19 @@ open class PullableBottomBar: UIViewController {
             self.expand()
             return
         }
-
+        
         let translation = recognizer.translation(in: view)
         let y = view.frame.minY
-
+        
         view.frame.origin.y = min(max(y + translation.y, pullableMinY), pullableMaxY)
-
+        
         if recognizer.state == .ended,
            !snapPoints.isEmpty {
+            
             let targetY = nearestPoint(of: y)
             let distance = abs(y - targetY)
-            let duration = max(min(distance / velocity.y, distance / (UIScreen.main.bounds.height / 3)), 0.3)
-
+            let duration = max(min(distance / velocity.y, distance / (UIScreen.main.bounds.height / 3)), 0.35)
             scroll(toY: targetY, duration: Double(duration))
         }
-    }
-}
-
-extension UIView{
-    func makeDefault(){
-        self.clipsToBounds = true
     }
 }
